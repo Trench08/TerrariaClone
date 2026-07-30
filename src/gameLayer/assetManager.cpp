c71af10f761b2dc22804c191ea78756a041602a1
Trench08 <103614943+Trench08@users.noreply.github.com>
2026-07-30 13:43:05 +0100

    Setup Finished

---
+++ src/gameLayer/assetManager.cpp
@@ -0,0 +1,10 @@
+//
+// Created by vsuni on 29/07/2026.
+//
+
+#include "assetManager.h"
+
+void assetManager::LoadAll()
+{
+    Atlas = LoadTexture(RESOURCES_PATH "/textures.png");
+}
---
+++ src/gameLayer/assetManager.h
@@ -0,0 +1,14 @@
+//
+// Created by vsuni on 29/07/2026.
+//
+#pragma once
+#include "raylib.h"
+
+
+class assetManager
+{
+    Texture2D Atlas = {};
+
+    void LoadAll();
+};
+
